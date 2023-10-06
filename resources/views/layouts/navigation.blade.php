b27d60da90404b0a70a2962f0869a8244283d5af
Jean <jeanmacario048@gmail.com>
2023-10-06 16:14:46 -0600

    Add\ refactor navigation menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('billing.payment_method_form')" :active="request()->routeIs('billing.payment_method_form')">
                         {{ __('Método de pago') }}
                     </x-nav-link>
@@ -34,7 +31,7 @@
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
-                            <div class="ml-1">
+                            <div class="hidden ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
@@ -43,6 +40,7 @@
                     </x-slot>
 
                     <x-slot name="content">
+                    <div class="hidden">
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Profile') }}
                         </x-dropdown-link>
@@ -57,6 +55,7 @@
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
+                    </div>
                     </x-slot>
                 </x-dropdown>
             </div>
@@ -76,9 +75,15 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-            <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+        <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex">
+            <x-nav-link :href="route('billing.payment_method_form')" :active="request()->routeIs('billing.payment_method_form')">
+                {{ __('Método de pago') }}
+            </x-responsive-nav-link>
+            <x-nav-link :href="route('billing.plans')" :active="request()->routeIs('billing.plans')">
+                {{ __('Planes') }}
+            </x-responsive-nav-link>
+            <x-nav-link :href="route('billing.my_subscription')" :active="request()->routeIs('billing.my_subscription')">
+                {{ __('Mi suscripción') }}
             </x-responsive-nav-link>
         </div>
 
@@ -89,7 +94,7 @@
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
-            <div class="mt-3 space-y-1">
+            <div class="hidden mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
