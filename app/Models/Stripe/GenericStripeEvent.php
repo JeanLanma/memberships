7b2e734397c79450640a4d64ab112f40efe324db
Jean <jeanmacario048@gmail.com>
2023-10-19 16:27:31 -0600

    Add\ Favicon

--- app/Models/Stripe/GenericStripeEvent.php
+++ app/Models/Stripe/GenericStripeEvent.php
@@ -3,12 +3,30 @@
 namespace App\Models\Stripe;
 
 use App\Interfaces\Descriptable;
+use App\Interfaces\Storageable;
 
-class GenericStripeEvent implements Descriptable {
+class GenericStripeEvent implements Storageable {
 
     public function GetDescription(): string
     {
         return "Generic Stripe Event";
     }
 
+    public function GetStoreObject(): object|array
+    {
+        return (object) [
+            'subscription_id' => 'generic',
+            'updated_at' => now()
+        ];
+    }
+
+    public function HasStoreUpdate(): bool
+    {
+        return false;
+    }
+
+    public function UpdateStore(): void
+    {
+        return;
+    }
 }
